2c91f1814e0d8e6a57c336fc5cc1359ccd718206
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2023-10-23 21:25:20 +0530

    Feature done 3.0.0 Live

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function convertToBase64() {
   const fileInput = document.getElementById('imageInput');
   const convertedImg = document.getElementById('convertedImg');
   const base64Output = document.getElementById('base64Output');
+  const copyToClipboardBtn = document.getElementById('copyToClipboardBtn');
 
   const file = fileInput.files[0];
   if (file) {
@@ -13,10 +14,43 @@ function convertToBase64() {
 
       const base64String = event.target.result.split(',')[1];
       base64Output.value = base64String;
+
+      copyToClipboardBtn.style.display = "block"
     };
 
     reader.readAsDataURL(file);
   } else {
     alert('Please select an image file.');
+  }
+}
+
+function copyToClipboard() {
+  const featureNotLive = document.getElementById('feature-not-live');
+  const textArea = document.getElementById('base64Output');
+  const copyBtn = document.getElementById("copyToClipboardBtn");
+
+  if (navigator.clipboard) {
+    const text = textArea.value
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        copyBtn.innerHTML = "Text Copied!";
+
+        setTimeout(function () {
+          copyBtn.innerHTML = "Copy to clipboard";
+        }, 2000);
+      })
+      .catch((err) => {
+        console.error('Failed to copy text: ', err);
+      });
+  } else {
+    // Fallback for browsers that do not support the Clipboard API
+    textArea.select();
+    document.execCommand("copy");
+
+    copyBtn.innerHTML = "Text Copied!";
+
+    setTimeout(function () {
+      copyBtn.innerHTML = "Copy to clipboard";
+    }, 2000);
   }
 }
