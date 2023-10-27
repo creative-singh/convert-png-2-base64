442abb9971c4b30d9e64cf5472c3cde020421902
Bhavleen Singh <singhbhavleen3@gmail.com>
2023-10-27 11:23:40 +0530

    Script File Updated

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 function convertToBase64() {
+  // Taking all the inputs/outputs
   const fileInput = document.getElementById('imageInput');
   const convertedImg = document.getElementById('convertedImg');
   const base64Output = document.getElementById('base64Output');
   const copyToClipboardBtn = document.getElementById('copyToClipboardBtn');
 
+  // Get File
   const file = fileInput.files[0];
   if (file) {
+    // Read File
     const reader = new FileReader();
 
-    reader.onload = function (event) {
+    reader.onload = (event) => {
+      // Showing image on screen
       convertedImg.src = event.target.result;
       convertedImg.style.display = 'block';
 
+      // Converting to base64
       const base64String = event.target.result.split(',')[1];
       base64Output.value = base64String;
 
+      // Copy to clipboard button
       copyToClipboardBtn.style.display = "block"
     };
 
@@ -25,10 +31,12 @@ function convertToBase64() {
 }
 
 function copyToClipboard() {
+  // Inputs 
   const featureNotLive = document.getElementById('feature-not-live');
   const textArea = document.getElementById('base64Output');
   const copyBtn = document.getElementById("copyToClipboardBtn");
 
+  // AS document.execCommand is deprecated, using navigator.clipboard
   if (navigator.clipboard) {
     const text = textArea.value
     navigator.clipboard.writeText(text)
